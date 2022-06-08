27680b57131c05c0aaff086d642551accc2d23ab
Papa96108 <78362404+Papa96108@users.noreply.github.com>
2022-06-08 08:52:45 +0000

    V1
    V1

---
+++ ProjetArchi/Sante.php
@@ -0,0 +1,12 @@
+<?php
+
+$con = mysqli_connect('localhost','root','','mglsi_news');
+
+$req = mysqli_query($con,'SELECT titre, contenu FROM article where categorie = 2');
+
+while($row = mysqli_fetch_assoc($req)){
+    echo $row['titre'].'<br>'.'<hr>';
+
+    echo $row['contenu'].'<br>';
+}
+?> 
